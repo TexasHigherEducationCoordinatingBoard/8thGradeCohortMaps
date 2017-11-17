d3b6f5f41ee5e04d42c2f5e3836d42ff6df93990
johndinning <johnbdinning@gmail.com>
2017-11-17 12:13:25 -0500

    Initial commit: Enrollment map target populations

---
+++ Enrollment/Enrollment.js
@@ -0,0 +1,42 @@
+const toggleableLayers = [
+    { ids: ['AApEnrollDiff', 'AApEnrollDiffText'], name: ['African American'] },
+    { ids: ['HisppEnrollDiff', 'HisppEnrollDiffText'], name: ['Hispanic'] },
+    { ids: ['AAmpEnrollDiff', 'AAmpEnrollDiffText'], name: ['African American Males'] },
+];
+
+function hideAllLayers() {
+    toggleableLayers.forEach(function (layer, i) {
+        var link = menu.children[i];
+        link.className = '';
+        layer.ids.forEach(function (layerId) {
+            map.setLayoutProperty(layerId, 'visibility', 'none');
+        });
+    });
+}
+
+
+function setOnLinkClickHandler(link, layer) {
+    link.onclick = function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        hideAllLayers();
+        this.className = 'active';
+        layer.ids.forEach(function (layerId) {
+            map.setLayoutProperty(layerId, 'visibility', 'visible');
+        });
+    };
+}
+
+function addLayerNav(map) {
+    toggleableLayers.forEach(function (layer) {
+        var link = document.createElement('a');
+        link.href = '#';
+        link.textContent = layer.name;
+
+        setOnLinkClickHandler(link, layer);
+
+        menu.appendChild(link);
+    });
+    document.getElementById("menu").firstChild.className = 'active';
+};
+
