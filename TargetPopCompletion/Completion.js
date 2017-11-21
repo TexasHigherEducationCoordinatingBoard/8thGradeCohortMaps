887ae0bef22b1bee3af11eb3ee7d971489f317b6
johndinning <johnbdinning@gmail.com>
2017-11-21 00:29:43 -0500

    place map in iframe

---
+++ TargetPopCompletion/Completion.js
@@ -0,0 +1,43 @@
+
+const toggleableLayers = [
+    { ids: ['AApCompDiff', 'AApCompDiffText'], name: ['African American'] },
+    { ids: ['HisppCompDiff', 'HisppCompDiffText'], name: ['Hispanic'] },
+    { ids: ['AAmpCompDiff', 'AAmpCompDiffText'], name: ['African American Males'] },
+];
+
+function hideAllLayers() {
+    toggleableLayers.forEach(function (layer, i) {
+        var link = menu.children[i];
+        link.className = '';
+        layer.ids.forEach(function (layerId) {
+            map.setLayoutProperty(layerId, 'visibility', 'none');
+        });
+    });
+}
+
+
+function setOnLinkClickHandler(link, layer) {
+    link.onclick = function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        hideAllLayers();
+        this.className = 'active';
+        layer.ids.forEach(function (layerId) {
+            map.setLayoutProperty(layerId, 'visibility', 'visible');
+        });
+    };
+}
+
+function addLayerNav(map) {
+    toggleableLayers.forEach(function (layer) {
+        var link = document.createElement('a');
+        link.href = '#';
+        link.textContent = layer.name;
+
+        setOnLinkClickHandler(link, layer);
+
+        menu.appendChild(link);
+    });
+    document.getElementById("menu").firstChild.className = 'active';
+};
+
