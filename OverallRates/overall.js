86097b9121d276433d6f3d2d3d72d31dfe90d060
johndinning <johnbdinning@gmail.com>
2017-11-26 00:37:06 -0500

    1st draft map of overall Enrollmt and Completion Rates

---
+++ OverallRates/overall.js
@@ -0,0 +1,43 @@
+bbox=[[-107.02, 25.62], [-93.38, 36.72]];
+
+const toggleableLayers = [
+    { ids: ['TotpEnroll', 'TotpEnrollText'], name: ['Enrollment Rates'] },
+    { ids: ['TotpComp', 'TotpCompText'], name: ['Completion Rates'] },
+];
+
+
+function hideAllLayers() {
+    toggleableLayers.forEach(function (layer, i) {
+        var link = menu.children[i];
+        link.className = '';
+        layer.ids.forEach(function (layerId) {
+            map.setLayoutProperty(layerId, 'visibility', 'none');
+        });
+    });
+}
+
+function setOnLinkClickHandler(link, layer) {
+    link.onclick = function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        hideAllLayers();
+        this.className = 'active';
+        layer.ids.forEach(function (layerId) {
+            map.setLayoutProperty(layerId, 'visibility', 'visible');
+        });
+    };
+}
+
+function addLayerNav(map) {
+    toggleableLayers.forEach(function (layer) {
+        var link = document.createElement('a');
+        link.href = '#';
+        link.textContent = layer.name;
+
+        setOnLinkClickHandler(link, layer);
+
+        menu.appendChild(link);
+    });
+    document.getElementById("menu").firstChild.className = 'active';
+};
+
