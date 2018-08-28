30cfe263e02c7bd745980d436b1605d284ec799d
John Dinning <john.dinning@thecb.state.tx.us>
2018-08-28 11:46:56 -0500

    cleanup javascript

--- README.md
+++ README.md
@@ -1,26 +1,23 @@
 # Maps of the 8th Grade Cohort Longitudinal Study
 
-### 60x30TX
+## 60x30TX
 Maps of enrollment and completion of higher education in Texas using data from the Texas Higher Education Coordinating Board's 
 11-year [8th Grade Cohort Longitudinal Study](http://www.txhighereddata.org/index.cfm?objectId=F2CBE4A0-C90B-11E5-8D610050560100A9).
 
 The overarching goal of the 2015-2030 [Texas Higher Education Strategic Plan](http://www.thecb.state.tx.us/reports/PDF/9306.PDF?CFID=57485581&CFTOKEN=60423954), 
-known as 60x30TX, is to increase the percent of the Texas population with a postsecondary credential. The rate at which Texas students successfully earn a 
+known as *60x30TX*, is to increase the percent of the Texas population with a postsecondary credential. The rate at which Texas students successfully earn a 
 college degree or certificate is a key contributor to this goal.
 
-### Target Populations
+## Target Populations
 The Strategic Plan identifies target populations that have had historically lower rates of college success. These 
 groups include African American, Economically Disadvantaged, Hispanic, and Male students. The 8th Grade Cohort maps show
 the distribution of these groups throughout the state as well as their enrollment and completion rates in higher education.
 
-### Analysis
+## Analysis
 The data comes from 8th Grade Cohort Longitudinal Study excel workbooks. 
 The data is processed using free, open source python libraries and merged to the Texas Education Agency service center region polygons using proprietary ArcGIS Pro python tools. The 
 same results could also be accomplished without ArcGIS Pro by using the open source GDAL library. The complete code 
 for processing and preparing the data can be found in [this Jupyter Notebook](https://github.com/TexasHigherEducationCoordinatingBoard/8thGradeCohortMaps/blob/master/8thGradeCohort2007DataPrep.ipynb).
 
-The maps were created and rendered in the browser using the Mapbox GL javascript library. For questions about this project,
-please contact:
-
-  John Dinning, Program Director in the division of Strategic Planning and Funding at the Texas Higher Education Coordinating Board. john.dinning@thecb.state.tx.us
+The maps were created and rendered in the browser using the Mapbox GL javascript library. For questions about this project, please contact John Dinning, Program Director in Strategic Planning and Funding at the Texas Higher Education Coordinating Board: john.dinning@thecb.state.tx.us.
 
--- TargetPopCompletion/Completion.js
+++ TargetPopCompletion/Completion.js
@@ -1,5 +1,229 @@
 bbox=[[-107.02, 25.62], [-93.38, 36.72]];
 
+//add map
+mapboxgl.accessToken = 'pk.eyJ1Ijoiam9obmRpbm5pbmciLCJhIjoiY2oxazR5NjNvMDFveDJ5b2FzbWZwbjFpbiJ9.bixlFwP8Kf43qHa7Z6XK8g';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: cohortStyle,
+  center: [-99.19, 31.30],
+  zoom: 5,
+  maxZoom: 7,
+  minZoom: 3,
+  maxBounds: [[-140, 15], [-60, 45]]
+});
+
+map.on('load', function () {
+  map.addSource('Cohort', {
+    'type': 'vector',
+    'url': 'mapbox://johndinning.9koks5ed'
+  });
+  map.addControl(new mapboxgl.NavigationControl({ position: 'top-right' }));
+  // Disable map rotation using right click + drag
+  map.dragRotate.disable();
+  // Disable map rotation using touch rotation gesture
+  map.touchZoomRotate.disableRotation();
+  addLayerNav(map);  // The funtions defined in the separate js doc
+  map.scrollZoom.disable(); //no mousewheel zoom
+  map.addLayer({
+    'id': 'AApComp',
+    'type': 'fill',
+    'source': 'Cohort',
+    'source-layer': 'CohortTEARegionPolys',
+    "layout": {
+      'visibility': 'visible'
+    },
+    'paint': {
+      'fill-color': {
+        property: 'AApComp',
+        type: 'exponential',
+        stops: [
+          [6, '#f0f9e8'],
+          [10, '#bae4bc'],
+          [14, '#7bccc4'],
+          [18, '#43a2ca'],
+          [22, '#0868ac']
+        ],
+      },
+    }
+  }, 'motorway-casing-Major'
+  );
+  map.addLayer({
+    "id": "AApCompText",
+    "type": "symbol",
+    "source": "Cohort",
+    "source-layer": "CohortTEARegionPoints",
+    "maxzoom": 6.4,
+    "layout": {
+      'visibility': 'visible',
+      "text-field": "{AApComp}%",
+      "text-font": [
+        "DIN Offc Pro Medium",
+        "Arial Unicode MS Bold"
+      ],
+      "text-size": 12
+    }
+  }
+  );
+  map.addLayer({
+    'id': 'EcopComp',
+    'type': 'fill',
+    'source': 'Cohort',
+    'source-layer': 'CohortTEARegionPolys',
+    "layout": {
+      'visibility': 'none'
+    },
+    'paint': {
+      'fill-color': {
+        property: 'EcopComp',
+        stops: [
+          [6, '#f0f9e8'],
+          [10, '#bae4bc'],
+          [14, '#7bccc4'],
+          [18, '#43a2ca'],
+          [22, '#0868ac']
+        ],
+      },
+    }
+  }, 'motorway-casing-Major'
+  );
+  map.addLayer({
+    "id": "EcopCompText",
+    "type": "symbol",
+    "source": "Cohort",
+    "source-layer": "CohortTEARegionPoints",
+    "maxzoom": 6.4,
+    "layout": {
+      'visibility': 'none',
+      "text-field": "{EcopComp}%",
+      "text-font": [
+        "DIN Offc Pro Medium",
+        "Arial Unicode MS Bold"
+      ],
+      "text-size": 12
+    }
+  }
+  );
+  map.addLayer({
+    'id': 'HispComp',
+    'type': 'fill',
+    'source': 'Cohort',
+    'source-layer': 'CohortTEARegionPolys',
+    "layout": {
+      'visibility': 'none'
+    },
+    'paint': {
+      'fill-color': {
+        property: 'HispComp',
+        stops: [
+          [6, '#f0f9e8'],
+          [10, '#bae4bc'],
+          [14, '#7bccc4'],
+          [18, '#43a2ca'],
+          [22, '#0868ac']
+        ],
+      },
+    }
+  }, 'motorway-casing-Major'
+  );
+  map.addLayer({
+    "id": "HispCompText",
+    "type": "symbol",
+    "source": "Cohort",
+    "source-layer": "CohortTEARegionPoints",
+    "maxzoom": 6.4,
+    "layout": {
+      'visibility': 'none',
+      "text-field": "{HispComp}%",
+      "text-font": [
+        "DIN Offc Pro Medium",
+        "Arial Unicode MS Bold"
+      ],
+      "text-size": 12
+    }
+  }
+  );      
+  map.addLayer({
+    'id': 'TotmpComp',
+    'type': 'fill',
+    'source': 'Cohort',
+    'source-layer': 'CohortTEARegionPolys',
+    "layout": {
+      'visibility': 'none'
+    },
+    'paint': {
+      'fill-color': {
+        property: 'TotmpComp',
+        stops: [
+          [6, '#f0f9e8'],
+          [10, '#bae4bc'],
+          [14, '#7bccc4'],
+          [18, '#43a2ca'],
+          [22, '#0868ac']
+        ],
+      },
+    }
+  }, 'motorway-casing-Major'
+  );
+  map.addLayer({
+    "id": "TotmpCompText",
+    "type": "symbol",
+    "source": "Cohort",
+    "source-layer": "CohortTEARegionPoints",
+    "maxzoom": 6.4,
+    "layout": {
+      'visibility': 'none',
+      "text-field": "{TotmpComp}%",
+      "text-font": [
+        "DIN Offc Pro Medium",
+        "Arial Unicode MS Bold"
+      ],
+      "text-size": 12
+    }
+  }
+  );
+  map.addLayer({
+    "id": "RegionLines",
+    "type": "line",
+    "source": "Cohort",
+    "source-layer": "CohortTEARegionPolys",
+    "layout": { 'visibility': 'visible' },
+    paint: {
+      "line-color": "#808080",
+      "line-width": 1,
+      "line-opacity": 0.3
+    }
+  });
+   map.addLayer({
+    "id": "TexasOutline",
+    "type": "line",
+    "source": {
+      'type': 'vector',
+      'url': 'mapbox://johndinning.7rzn4kl6'
+      },
+    'source-layer': 'Texas-d92wyb',
+    "paint": {
+      "line-color": "rgba(0,0,0, 1)",
+      "line-width": 2,
+      "line-opacity": 0.7
+    }
+  }, 'motorway-casing-Major');
+  map.addLayer({
+    "id": "highlight-hover",
+    "type": "fill",
+    "source": "Cohort",
+    'source-layer': 'CohortTEARegionPolys',
+    "paint": {
+      "fill-color": "#99ff99",
+      "fill-opacity": .8
+    },
+    "filter": ['==', "TEAReg", ""]
+  }, 'motorway-casing-Major');
+  map.fitBounds(bbox, linear = true); //fit Texas at any zoom and ease to
+});
+
+
+
+//create toggle menu
 const toggleableLayers = [
     { ids: ['AApComp', 'AApCompText'], name: ['African American'] },
     { ids: ['EcopComp', 'EcopCompText'], name: ['Economically Disadvantaged'] },
@@ -46,3 +270,68 @@ function addLayerNav(map) {
     document.getElementById("menu").firstChild.className = 'active';
 };
 
+
+//create popups
+
+
+var popup = new mapboxgl.Popup({
+    closeButton: false,
+    closeOnClick: false
+  });
+
+
+  map.on("mousemove", "AApComp", function (e) {
+    map.setFilter("highlight-hover", ["==", "TEAReg", e.features[0].properties["TEAReg"]]);
+    map.getCanvas().style.cursor = 'pointer';
+    popup.setLngLat(e.lngLat)
+      .setHTML('<strong>Completion Rates for the </strong><strong style="font-style: italic;">' + e.features[0].properties.RegName + ' Region</strong><table><tr style="font-weight: bold;"><td>Overall Cohort: </td><td>' + e.features[0].properties.TotpComp.toFixed(1) + '%</td></tr><tr><td>African American: </td><td>' + e.features[0].properties.AApComp.toFixed(1) + '%</td></tr><tr><td>Hispanic: </td><td>' + e.features[0].properties.HispComp.toFixed(1) + '%</td></tr><tr><td>Economic Dis.: </td><td>' + e.features[0].properties.EcopComp.toFixed(1) + '%</td></tr><tr><td>Males: </td><td>' + e.features[0].properties.TotmpComp.toFixed(1) + '%</td></tr></p>')
+      .addTo(map);
+  });
+
+  map.on("mouseleave", "AApComp", function (e) {
+    map.setFilter("highlight-hover", ["==", "TEAReg", ""]);
+    map.getCanvas().style.cursor = '';
+    popup.remove();
+  });
+
+  map.on("mousemove", "EcopComp", function (e) {
+    map.setFilter("highlight-hover", ["==", "TEAReg", e.features[0].properties["TEAReg"]]);
+    map.getCanvas().style.cursor = 'pointer';
+    popup.setLngLat(e.lngLat)
+      .setHTML('<strong>Completion Rates for the </strong><strong style="font-style: italic;">' + e.features[0].properties.RegName + ' Region</strong><table><tr style="font-weight: bold;"><td>Overall Cohort: </td><td>' + e.features[0].properties.TotpComp.toFixed(1) + '%</td></tr><tr><td>African American: </td><td>' + e.features[0].properties.AApComp.toFixed(1) + '%</td></tr><tr><td>Hispanic: </td><td>' + e.features[0].properties.HispComp.toFixed(1) + '%</td></tr><tr><td>Economic Dis.: </td><td>' + e.features[0].properties.EcopComp.toFixed(1) + '%</td></tr><tr><td>Males: </td><td>' + e.features[0].properties.TotmpComp.toFixed(1) + '%</td></tr></p>')
+      .addTo(map);
+  });
+
+  map.on("mouseleave", "EcopComp", function (e) {
+    map.setFilter("highlight-hover", ["==", "TEAReg", ""]);
+    map.getCanvas().style.cursor = '';
+    popup.remove();
+  });
+
+  map.on("mousemove", "HispComp", function (e) {
+    map.setFilter("highlight-hover", ["==", "TEAReg", e.features[0].properties["TEAReg"]]);
+    map.getCanvas().style.cursor = 'pointer';
+    popup.setLngLat(e.lngLat)
+      .setHTML('<strong>Completion Rates for the </strong><strong style="font-style: italic;">' + e.features[0].properties.RegName + ' Region</strong><table><tr style="font-weight: bold;"><td>Overall Cohort: </td><td>' + e.features[0].properties.TotpComp.toFixed(1) + '%</td></tr><tr><td>African American: </td><td>' + e.features[0].properties.AApComp.toFixed(1) + '%</td></tr><tr><td>Hispanic: </td><td>' + e.features[0].properties.HispComp.toFixed(1) + '%</td></tr><tr><td>Economic Dis.: </td><td>' + e.features[0].properties.EcopComp.toFixed(1) + '%</td></tr><tr><td>Males: </td><td>' + e.features[0].properties.TotmpComp.toFixed(1) + '%</td></tr></p>')
+      .addTo(map);
+  });
+
+  map.on("mouseleave", "HispComp", function (e) {
+    map.setFilter("highlight-hover", ["==", "TEAReg", ""]);
+    map.getCanvas().style.cursor = '';
+    popup.remove();
+  });
+
+  map.on("mousemove", "TotmpComp", function (e) {
+    map.setFilter("highlight-hover", ["==", "TEAReg", e.features[0].properties["TEAReg"]]);
+    map.getCanvas().style.cursor = 'pointer';
+    popup.setLngLat(e.lngLat)
+      .setHTML('<strong>Completion Rates for the </strong><strong style="font-style: italic;">' + e.features[0].properties.RegName + ' Region</strong><table><tr style="font-weight: bold;"><td>Overall Cohort: </td><td>' + e.features[0].properties.TotpComp.toFixed(1) + '%</td></tr><tr><td>African American: </td><td>' + e.features[0].properties.AApComp.toFixed(1) + '%</td></tr><tr><td>Hispanic: </td><td>' + e.features[0].properties.HispComp.toFixed(1) + '%</td></tr><tr><td>Economic Dis.: </td><td>' + e.features[0].properties.EcopComp.toFixed(1) + '%</td></tr><tr><td>Males: </td><td>' + e.features[0].properties.TotmpComp.toFixed(1) + '%</td></tr></p>')
+      .addTo(map);
+  });
+
+  map.on("mouseleave", "TotmpComp", function (e) {
+    map.setFilter("highlight-hover", ["==", "TEAReg", ""]);
+    map.getCanvas().style.cursor = '';
+    popup.remove();
+  });
